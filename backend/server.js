b3ba63dff9552bb1b590844417fa7b8131bc2567
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-09-25 23:02:05 -0400

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
