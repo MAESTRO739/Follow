ae03c926d98109b0c1239e03a45d6f28e7422183
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-09-26 17:14:21 -0400

    implemented follow and unfollow router, created protectRoute middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,4 +74,35 @@ const logoutUser = (req, res) => {
   }
 }
 
-export { signupUser, loginUser, logoutUser };
+const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToFollow = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id) {
+      return res.status(400).json({message: "You can't follow yourself"});
+    }
+    
+    if (!userToFollow || !currentUser) { return res.status(404).json({message: "User not found"}); }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // Unfollow user
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      res.status(200).json({message: "User unfollowed successfully"});
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      res.status(200).json({message: "User followed successfully"});
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log('Error in followUnfollowUser: ', error.message)
+  }
+}
+ 
+export { signupUser, loginUser, logoutUser, followUnfollowUser };
