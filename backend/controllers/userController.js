21692680e9801d679edd38b5d8c69ea37ab60e52
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-09-26 15:44:34 -0400

    implemented login route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,7 @@ const signupUser = async (req, res) => {
 
     if (newUser) {
       generateTokenAndSetCookie(newUser._id, res);
-      
+
       res.status(201).json({
         _id: newUser._id,
         name: newUser.name,
@@ -39,6 +39,29 @@ const signupUser = async (req, res) => {
     res.status(500).json({ message: error.message });
     console.log('Error in signupUser: ', error.message)
   }
-};
+}
 
-export default signupUser;
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPasswordCorrect) { return res.status(400).json({message: "Invalid username or password"}); }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username
+    })
+    
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log('Error in loginUser: ', error.message)
+  }
+}
+
+export { signupUser, loginUser };
