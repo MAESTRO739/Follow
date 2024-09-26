b9424cc87d616e667f6fd21135a68d5e59d44387
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-09-26 15:52:15 -0400

    implemented logout route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,4 +64,14 @@ const loginUser = async (req, res) => {
   }
 }
 
-export { signupUser, loginUser };
+const logoutUser = (req, res) => {
+  try {
+    res.cookie('jwt', '', {maxAge: 1});
+    res.status(200).json({message: 'Logged out successfully'});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log('Error in logoutUser: ', error.message)
+  }
+}
+
+export { signupUser, loginUser, logoutUser };
