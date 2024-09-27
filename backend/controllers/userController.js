531852aa74af86035519936c2a3c85b882a39500
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-09-26 20:21:09 -0400

    implemented get profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,19 @@ import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
 import generateTokenAndSetCookie from '../utils/helpers/generateTokenAndSetCookie.js';
 
+const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username }).select("-password").select("-updatedAt");
+    if (!user) { return res.status(404).json({message: "User not found"}); }
+    
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log('Error in getUserProfile: ', error.message)
+  }
+}
+
 const signupUser = async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
@@ -136,4 +149,4 @@ const updateUser = async (req, res) => {
   }
 }
 
-export { signupUser, loginUser, logoutUser, followUnfollowUser, updateUser };
+export { getUserProfile, signupUser, loginUser, logoutUser, followUnfollowUser, updateUser };
