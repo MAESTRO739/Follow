a665d7bb9c2f915dcfe0026ba137560fe29f3861
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-09-27 18:20:59 -0400

    redirect not logged in user from home to auth, and redirect logged in user from auth to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import { Container } from '@chakra-ui/react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import UserPage from './pages/UserPage'
 import PostPage from './pages/PostPage'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
+import { useRecoilValue } from 'recoil'
+import userAtom from './atoms/userAtom'
 
 function App() {
+  const user = useRecoilValue(userAtom);
+
   return (
     <Container maxW={{ base: '100%', sm: '90%', md: '85%', lg: '672px' }} mb={16}>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/auth" element={<AuthPage />}/>
+        <Route path="/" element={user ? <HomePage /> : <Navigate to={'auth'} />}/>
+        <Route path="/auth" element={user ? <Navigate to={'/'} /> : <AuthPage />}/>
+
         <Route path="/:username" element={<UserPage />}/>
         <Route path="/:username/post/:pid" element={<PostPage />}/>
       </Routes>
